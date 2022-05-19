68563e3cef36e91fc520285a3aea2baa16608661
Pavel Pak <pak.pavel1985@gmail.com>
2022-05-19 15:57:51 +0900

    Completed lesson123 Elements and Components

--- section6/lesson121-create react app/my-app/src/App.js
+++ section6/lesson121-create react app/my-app/src/App.js
@@ -1,18 +1,55 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const Header = () => {
+  return <h2>Hello World!</h2>
+}
+
+const Field = () => {
+  const holder = 'Enter here';
+  const styledField = {
+    width: '300px'
+  }
+  return <input type="text" 
+                placeholder={holder} 
+                style={styledField}/>
+}
+
+class Field2 extends React.Component {
+  render(){
+    const holder = 'Enter here';
+    const styledField = {
+      width: '300px'
+    }
+
+    return <input type="text" 
+                  placeholder={holder} 
+                  style={styledField}/>
+  }
+}
+
+function Btn(){
+  const text = 'Log in';
+  const logged = true;
+
+  // const res = () => {
+  //   return 'Log in';
+  // }
+
+  return <button>{logged ? 'Enter' : text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-      </header>
+        <Header/>
+        <Field/>
+        <Btn/>
+        <Field2/>
     </div>
   );
 }
 
+export {Header};
 export default App;
--- section6/lesson121-create react app/my-app/src/index.js
+++ section6/lesson121-create react app/my-app/src/index.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
+import { Header } from './App';
 
 
-// const elem = <h2>Hello World!</h2>;//JSX
 
-// const elem = React.createElement('h2', {className: 'greeting'}, 'Bye bye World!');//JS
-
-const text = 'Bye bye World'
-
-const elem = (
-  <div>
-    <h2>{text}</h2>
-    <input type="text" />
-    <button>Click</button>
-  </div>
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
