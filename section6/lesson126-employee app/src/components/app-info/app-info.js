afc2304e69f41fe65bc83b26dd47a13f082c8187
Pavel Pak <pak.pavel1985@gmail.com>
2022-05-20 10:06:36 +0900

    Completed lesson126 New Project Creation

---
+++ section6/lesson126-employee app/src/components/app-info/app-info.js
@@ -0,0 +1,14 @@
+import './app-info.css';
+
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employee's registry in the Monster Inc.</h1>
+            <h2>Total number of employees: </h2>
+            <h2>Upcomig bonus for: </h2>
+        </div>
+    );
+}
+
+export default AppInfo;
