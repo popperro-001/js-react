d1a09e4ebd6deb75a9d642230ec0d6f13f0d8496
popperro-001 <talkin.watermelon@gmail.com>
2022-07-21 13:49:32 +0900

    Completed lesson142

--- section6/lesson126-employee app/src/components/app-filter/app-filter.js
+++ section6/lesson126-employee app/src/components/app-filter/app-filter.js
@@ -5,20 +5,22 @@ const AppFilter = (props) => {
     const {filter, onFilterSelect} = props;
 
     const buttonsData = [
-        {name: 'all', label: 'All employees'},
-        {name: 'promotion', label: 'For promotion'},
-        {name: 'moreThan1000', label: 'Salary more $1000'}
+        {name: 'all', label: 'All employees', colored: false},
+        {name: 'promotion', label: 'For promotion', colored: false},
+        {name: 'moreThan1000', label: 'Salary more $1000', colored: true}
     ]
 
-    const buttons = buttonsData.map(({name, label}) => {
+    const buttons = buttonsData.map(({name, label, colored}) => {
         const active = filter === name;
         const clazz = active ? 'btn-light' : 'btn-outline-light';
+        const style = colored ? {color: 'red'} : null;
         return (
             <button 
                 className={`btn ${clazz}`}
                 type="button"
                 key={name}
-                onClick={() => onFilterSelect(name)}>
+                onClick={() => onFilterSelect(name)}
+                style={style}>
                 {label}
             </button>
         )
