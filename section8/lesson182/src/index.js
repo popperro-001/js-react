a154bb4ce9ed23b323aa7e456bbc2aa37e73dd8c
popperro-001 <talkin.watermelon@gmail.com>
2022-08-17 09:42:38 +0900

    Completed lesson182

---
+++ section8/lesson182/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import CssTransition from './CssTransition';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+    // <App/>,
+    <CssTransition/>,
+  document.getElementById('root')
+);
+
+
