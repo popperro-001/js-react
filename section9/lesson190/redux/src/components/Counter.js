8533f28741c538db440af8fe662f22a14b1acec5
popperro-001 <talkin.watermelon@gmail.com>
2022-08-29 16:29:50 +0900

    Completed lesson195

--- section9/lesson190/redux/src/components/Counter.js
+++ section9/lesson190/redux/src/components/Counter.js
@@ -1,27 +1,19 @@
-import { connect } from "react-redux";
-// import { bindActionCreators } from "redux";
-import * as actions from '../actions';
+import { useSelector, useDispatch } from "react-redux";
+import {inc, dec, res, rnd} from '../actions';
 
-const Counter = ({counter, inc, dec, rnd, res}) => {
+const Counter = () => {
+    const counter = useSelector(state => state.counter);
+    const dispatch = useDispatch();
+
     return (
         <div className="jumbotron">
             <h1>{counter}</h1>
-            <button onClick={res} className="btn btn-primary">RES</button>
-            <button onClick={dec} className="btn btn-danger">DEC</button>
-            <button onClick={inc} className="btn btn-success">INC</button>
-            <button onClick={rnd} className="btn btn-warning">RND</button>
+            <button onClick={() => dispatch(res())} className="btn btn-primary">RES</button>
+            <button onClick={() => dispatch(dec())} className="btn btn-danger">DEC</button>
+            <button onClick={() => dispatch(inc())} className="btn btn-success">INC</button>
+            <button onClick={() => dispatch(rnd())} className="btn btn-warning">RND</button>
         </div>
     )
 };
 
-const mapStateToProps = (state) => {
-    return {
-        counter: state.value
-    }
-};
-
-// const mapDispatchToProps = (dispatch) => {
-//     return  bindActionCreators(actions, dispatch)    
-// };
-
-export default connect(mapStateToProps, actions)(Counter);
+export default Counter;
--- section9/lesson190/redux/src/reducer.js
+++ section9/lesson190/redux/src/reducer.js
@@ -1,26 +1,26 @@
-const initialState = {value: 0};
+const initialState = {counter: 0};
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
     case 'INC':
       return {
         ...state,
-        value: state.value + 1
+        counter: state.counter + 1
       };
     case 'DEC':
       return {
         ...state,
-        value: state.value - 1
+        counter: state.counter - 1
       };
     case 'RND':
       return {
         ...state,
-        value: state.value * action.payload
+        counter: state.counter * action.payload
       };
     case 'RES':
       return {
         ...state,
-        value: state.value = initialState.value
+        counter: state.counter = initialState.counter
       };
     default:
       return state;
