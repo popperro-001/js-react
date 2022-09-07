1841d898d4f2bbf39c04fea4c1372609ab9cca08
popperro-001 <talkin.watermelon@gmail.com>
2022-09-07 10:28:11 +0900

    Typescript. Basic Props

--- react + typescript/react-typescript-demo/src/App.tsx
+++ react + typescript/react-typescript-demo/src/App.tsx
@@ -1,10 +1,33 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne',
+  }
+
+  const nameList = [
+    {
+      first: 'Bruce',
+      last: 'Banner',
+    },
+    {
+      first: 'Tony',
+      last: 'Stark',
+    },
+    {
+      first: 'Steve',
+      last: 'Rojers',
+    }
+  ]
   return (
     <div className="App">
-      <Greet name='Pavel'/>
+      <Greet name='Pavel' messageCount={20} isLoggedIn={false}/>
+      <Person name={personName}/>
+      <PersonList names={nameList}/>
     </div>
   );
 }
