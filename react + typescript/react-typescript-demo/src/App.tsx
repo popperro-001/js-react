a0972939fcfee15c58ad722b3671bcb67e5bc8be
popperro-001 <talkin.watermelon@gmail.com>
2022-09-07 13:11:40 +0900

    Typescript. Event Props

--- react + typescript/react-typescript-demo/src/App.tsx
+++ react + typescript/react-typescript-demo/src/App.tsx
@@ -1,18 +1,15 @@
 import './App.css';
-import { Status } from './components/Status';
-import { Heading } from './components/Heading';
-import { Oscar } from './components/Oscar';
-import { Greet } from './components/Greet';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
+
 
 function App() {
   return (
     <div className="App">
-      <Status status='loading'/>
-      <Heading>Text</Heading>
-      <Oscar>
-        <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
-      </Oscar>
-      <Greet name='Pavel' messageCount={15} isLoggedIn={true}/>
+      <Button handleClick={(event, id) => {
+        console.log('Button clicked', event, id)
+      }}/>
+      <Input value='' handleChange={(event) => console.log(event)}/>
     </div>
   );
 }
