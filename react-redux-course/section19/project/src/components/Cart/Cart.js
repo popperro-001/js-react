706d2bc3ad1cda9ee801c882f0433679c382af59
popperro-001 <talkin.watermelon@gmail.com>
2023-04-26 13:30:50 +0900

    Completed section19

---
+++ react-redux-course/section19/project/src/components/Cart/Cart.js
@@ -0,0 +1,22 @@
+import { useSelector } from "react-redux";
+
+import Card from "../UI/Card";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
+
+const Cart = (props) => {
+  const items = useSelector((state) => state.cart.items);
+
+  return (
+    <Card className={classes.cart}>
+      <h2>Your Shopping Cart</h2>
+      <ul>
+        {items.map((element) => (
+          <CartItem key={element.id} item={element} />
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default Cart;
---
+++ react-redux-course/section19/project/src/components/Cart/CartItem.js
@@ -0,0 +1,47 @@
+import { useDispatch } from "react-redux";
+
+import { cartActions } from "../../store/cart-slice";
+import classes from "./CartItem.module.css";
+
+const CartItem = (props) => {
+  const dispatch = useDispatch();
+
+  const { name, quantity, totalPrice, price, id } = props.item;
+
+  const addItemToCartHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title: name,
+        price,
+      })
+    );
+  };
+
+  const removeItemFromCartHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
+
+  return (
+    <li className={classes.item}>
+      <header>
+        <h3>{name}</h3>
+        <div className={classes.price}>
+          ${totalPrice.toFixed(2)}{" "}
+          <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
+        </div>
+      </header>
+      <div className={classes.details}>
+        <div className={classes.quantity}>
+          x <span>{quantity}</span>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={removeItemFromCartHandler}>-</button>
+          <button onClick={addItemToCartHandler}>+</button>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
